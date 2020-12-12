dbb65de5c9f14ff3783c3e67e1d08eb611398c4e
wennyray <50888197+wennyray@users.noreply.github.com>
2020-12-12 12:25:21 +0800

    Update prototype.py

--- prototype_python/prototype.py
+++ prototype_python/prototype.py
@@ -11,11 +11,19 @@ def prompt():
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
+       
       city = str(request.form['cityname'])
       url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d8c155b6234b83e3ca5d68e3e5a6deed"
       response = requests.get(url)
       data = response.text
-      parsed = json.loads(data)
+      weather_data = json.loads(data)
+
+      
+      state = str(request.form['state'])
+      url = "https://api.schooldigger.com/v1.2/rankings/schools/"+state+"?appID=f14c813a&appKey=560e6e0400648906909977e5c7ff3a43"
+      response = request.get(url)
+      data = response.text
+      school_data = json.loads(data)
       
       return render_template('form_submission.html')
         
