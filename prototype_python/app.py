356215150b1071ef33e5aead86254f1d940b990b
wennyray <50888197+wennyray@users.noreply.github.com>
2020-12-12 12:24:55 +0800

    Update app.py

--- prototype_python/app.py
+++ prototype_python/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '<h1>Hello!</h1>'
+def prompt():
+    return render_template('form_prompt.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+       
+      city = str(request.form['cityname'])
+      url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d8c155b6234b83e3ca5d68e3e5a6deed"
+      response = requests.get(url)
+      data = response.text
+      weather_data = json.loads(data)
+
+      
+      state = str(request.form['state'])
+      url = "https://api.schooldigger.com/v1.2/rankings/schools/"+state+"?appID=f14c813a&appKey=560e6e0400648906909977e5c7ff3a43"
+      response = request.get(url)
+      data = response.text
+      school_data = json.loads(data)
+      
+      return render_template('form_submission.html')
+        
+if __name__ == '__main__':
+    app.run(debug = True)
